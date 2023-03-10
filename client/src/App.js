af648e4e2f0164c64ca7b67a26b12d841099ddfb
Charles Peace <charlespeace@hotmail.com>
2023-03-09 20:59:36 -0800

    organize post/posts/form heirarchy and ensure each component renders on client side react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import openbook from './images/openbook.png'
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar position='static'>
+                <Typography variant='h2' align='center'>Open Book</Typography>
+                <img src={openbook} alt='openbook' height='60' width='60' />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 };
 
