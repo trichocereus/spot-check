6eff6f4cd25f2b8a75ecac63263afb676d6aaf51
Charles Peace <charlespeace@hotmail.com>
2023-03-12 18:39:41 -0700

    pass data using fetch/dispatch for create post and ensure creating new post actually updates to mongodb cluster

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
 import useStyles from './styles';
+import { createPost } from '../../actions/posts';
+
 
 const Form = () => {
     const [postData, setPostData] = useState({
@@ -11,10 +14,14 @@ const Form = () => {
         tags: '', 
         selectedFile: ''
     })
+
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        dispatch(createPost(postData));
     }
 
     const clear = () => {
