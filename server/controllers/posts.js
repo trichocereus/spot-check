42964083fee9b38731eb562abbf7c2e27c8682ec
Charles Peace <charlespeace@hotmail.com>
2023-03-13 17:57:41 -0700

    debug so updating form updates posts after clicking edit/submit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -32,6 +34,7 @@ export const updatePost = async(req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
     res.json(updatedPost);
 }
