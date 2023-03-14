5ffcaa1539b2cf5ac7edf7f4d05c79c8f36650e1
Charles Peace <charlespeace@hotmail.com>
2023-03-13 17:39:22 -0700

    edit form post and posts components to pass currentdata to form for updating

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
 import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
 
 
-const Form = ( currentId, setCurrentId) => {
+const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
         creator: '', 
         title: '', 
@@ -15,8 +16,13 @@ const Form = ( currentId, setCurrentId) => {
         selectedFile: ''
     })
 
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(post) setPostData(post);
+    }, [post])
 
     const handleSubmit = (e) => {
         e.preventDefault();
