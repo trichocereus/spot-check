24f3975345110aa4393f1def5d50718ae18f1379
Charles Peace <charlespeace@hotmail.com>
2023-03-20 13:31:22 -0700

    remove npm from root directory

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// const CONNECTION_URL = 'mongodb+srv://admin:admin123@cluster0.8bfdrez.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
