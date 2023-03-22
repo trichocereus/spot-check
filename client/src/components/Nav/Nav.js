ff6252502d5b400fecd166d3b84b7f7430050800
Charles Peace <charlespeace@hotmail.com>
2023-03-22 12:17:51 -0700

    restructure app.js to efficiently use react-router, create login button that uses switch from router to render content

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { AppBar, Typography, Avatar, Toolbar, Button } from '@material-ui/core';
 
 import useStyles from './styles';
 import pin from '../../images/pin.png'
@@ -7,10 +8,25 @@ import pin from '../../images/pin.png'
 const Nav = () => {
     const classes = useStyles();
 
+    const user = null;
+
     return(
     <AppBar className={classes.appBar} position='static' color ='inherit'>
-    <Typography className={classes.heading} variant='h4' align='center'>Spot Check</Typography>
-    <img className={classes.image} src={pin} alt='pin' height='35' />
+    <div className={classes.brandContainer}>
+        <Typography component={Link} to='/' className={classes.heading} variant='h4' align='center'>Spot Check</Typography>
+        <img className={classes.image} src={pin} alt='pin' height='35' />
+    </div>
+    <Toolbar className={classes.toolbar}>
+        {user ? (
+            <div className={classes.profile}>
+                <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
+                <Button varianet='contained' className={classes.logout} color='secondary'>Log Out</Button>
+            </div>
+        ) : (
+            <Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
+        )}
+    </Toolbar>
     </AppBar>
     );
 };
--- client/src/components/Nav/styles.js
+++ client/src/components/Nav/styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
 
 export default makeStyles((theme) => ({
   appBar: {
@@ -18,4 +19,26 @@ export default makeStyles((theme) => ({
   image: {
     marginLeft: '15px',
   },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '400px',
+  },
+  profile: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '400px',
+  },
+  userName: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  brandContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  }
 }));
