d55f7a86a032c77a51d65c72e6ef293e34d363db
Charles Peace <charlespeace@hotmail.com>
2023-03-27 23:24:06 -0700

    debug

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -26,12 +26,12 @@ export const signup = async (req, res) => {
         const existingUser = await User.findOne({ email });
         if (existingUser) return res.status(400).json({ message: "User already exists."});
 
-        if (password !== confirmPassword) return resizeTo.status(400).json({ message: "Passwords don't match." });
+        if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match." });
     
         const hashedPassword = await bcrypt.hash(password, 12);
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
         const token = jwt.sign({ email: result.email, id: result._id}, 'test', { expiresIn: '1hr' });
-        res.status(200).json({ result: result, token });
+        res.status(200).json({ result, token });
     } catch {
         res.status(500).json({ message: "Something went wrong."})
     }
